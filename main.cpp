948cbb7d83a5e2da8398c7e657f09a5ef4ef8fc9
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-03-21 21:07:06 +1300

    Create blank window with GLFW and GLAD.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,63 @@
+//  EconSimPlusPlus
+//  A program that simulates the historical development of capitalist economies.
+//  Copyright (C) 2024.   Anthony Dickson anthony.dickson9656@gmail.com
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //
 // Created by Anthony on 31/10/2023.
 //
 #include <iostream>
 
+#include "glad/include/glad/glad.h"
+#include "glfw/include/GLFW/glfw3.h"
+
+void framebuffer_size_callback(GLFWwindow*, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD. " << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
