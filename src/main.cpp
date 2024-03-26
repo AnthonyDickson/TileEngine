c6a7d7844d43068dcf54790685442aef13fc094a
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-03-26 19:45:58 +1300

    Spin a camera around many cubes spinning around.

--- src/main.cpp
+++ src/main.cpp
@@ -152,7 +152,7 @@ int main() {
             glm::vec3(-1.3f, 1.0f, -1.5f)
     };
 
-    auto view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+    const float radius{10.0f};
     auto projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
 
     while (!glfwWindowShouldClose(window)) {
@@ -161,6 +161,12 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
+
+        float camX{static_cast<float>(sin(glfwGetTime()) * radius)};
+        float camZ{static_cast<float>(cos(glfwGetTime()) * radius)};
+        auto view = glm::lookAt(glm::vec3(camX, 0.0f, camZ),
+                                glm::vec3(0.0f, 0.0f, 0.0f),
+                                glm::vec3(0.0f, 1.0f, 0.0f));
 
         shader.use();
         shader.setMat4("view", view);
