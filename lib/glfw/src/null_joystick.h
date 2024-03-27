b4795ba24acc4c483ebca850716e0266fb01fe6c
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-03-27 19:52:33 +1300

    Move GLFW to CMake dependency.

--- lib/glfw/src/null_joystick.h
+++
@@ -1,8 +0,0 @@
-
-
-GLFWbool _glfwInitJoysticksNull(void);
-void _glfwTerminateJoysticksNull(void);
-GLFWbool _glfwPollJoystickNull(_GLFWjoystick* js, int mode);
-const char* _glfwGetMappingNameNull(void);
-void _glfwUpdateGamepadGUIDNull(char* guid);
-
