ff35aaf61f1d49652e8804ee0f691d6ceec42fc3
Anthony Dickson <dican732@student.otago.ac.nz>
2024-05-06 11:16:08 +1200

    Add stb image resize and write libraries.

--- lib/stb_image/stb_image.cpp
+++ lib/stb_image/stb_image.cpp
@@ -21,5 +21,10 @@
 
 // The below directive and include statements basically copy in the definitions for the stb_image library.
 #define STB_IMAGE_IMPLEMENTATION
-
 #include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
